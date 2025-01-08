d604b5fcab3a3ee6d1ac8b87577c9f602530627f
Dastan Akhmetov <dnaseven@proton.me>
2025-01-08 05:57:13 +0500

    Add error classes

---
+++ src/errors/base.ts
@@ -0,0 +1,7 @@
+export class BaseError extends Error {}
+
+export class BaseApiError extends BaseError {}
+
+export class BaseStoreError extends BaseError {}
+
+export class BaseUiError extends BaseError {}
